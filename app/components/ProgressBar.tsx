53b9e6061beb4e5992b943fca32c0a2febd07b5b
Tayyaba-create <tayyabaf975@gmail.com>
2025-12-26 11:03:01 +0500

    FLWAPP-005-responsive gme page header

--- app/components/ProgressBar.tsx
+++ app/components/ProgressBar.tsx
@@ -13,7 +13,10 @@ export default function ProgressBar({
 
   return (
     <div className="w-full px-0 py-4 mt-[40px]">
-      <div className="relative w-full h-[20px] bg-[#0F1125] my-[5px] box-border" style={{ boxShadow: "inset 0 0 0 3px #3A8DFF" }}>
+      <div
+        className="relative w-full h-[20px] bg-[#0F1125] my-[5px] box-border"
+        style={{ boxShadow: "inset 0 0 0 3px #3A8DFF" }}
+      >
         {/* Progress Bar Fill */}
         <div
           className="h-full bg-gradient-to-r from-[#7B61FF] to-[#3A8DFF] transition-all duration-500 ease-in-out relative"
@@ -26,7 +29,7 @@ export default function ProgressBar({
         <div
           className="absolute top-1/2 -translate-y-1/2 w-[100px] h-[100px] bg-[#0F1125] mt-[15px] mb-[60px] rounded-full border-[2px] border-[#3A8DFF] flex items-center justify-center transition-all duration-500 ease-in-out overflow-visible"
           style={{
-            left: `calc(${progressPercentage}% - 50px)`,
+            left: `calc(${progressPercentage}% - 10px)`,
             boxShadow: "0 0 30px #3A8DFF",
           }}
         >
